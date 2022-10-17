963df8f0768ec5c00c21b728b15198ab1ebbe4df
amstelart <amsteldroid@gmail.com>
2022-10-17 10:42:21 +0300

    dev-01

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,7 +1,16 @@
 // Если на проекте jQuery
-// $( document ).ready(function() {
-//   // code
-// });
+$( document ).ready(function() {
+  // code
+
+    $(".main-slider").owlCarousel({
+        items: 1,
+        nav: true,
+        margin: 10,
+        dots: true,
+        loop: true,
+        center: true
+    });
+});
 
 // Изоляция без jQuery
 // (function(){
